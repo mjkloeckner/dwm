be0cc94e54c4f3db891b83427edeb2c6e2974d78
klewer-martin <martin.cachari@gmail.com>
2022-03-27 22:17:39 -0300

    update

--- config.def.h
+++ config.def.h
@@ -2,6 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 3;        /* border pixel of windows */
+static const Gap default_gap        = {.isgap = 1, .realgap = 20, .gappx = 20};
 static const unsigned int snap      = 16;       /* snap pixel */
 static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
 static const unsigned int systrayonleft = 0;   	/* 0: systray in the right corner, >0: systray on left of status text */
@@ -10,8 +11,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 0;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Source Code Pro:style=Regular:size=8", "JoyPixels:style=Regular:size=7:antialias=true:autohint=true", "DejaVu Sans" };
-static const char dmenufont[]       = "DejaVuSansMono Nerd Font:style=Regular:size=8";
+static const char *fonts[]          = { "Source Code Pro:style=Regular:size=9", "JoyPixels:style=Regular:size=8:antialias=true:autohint=true", "DejaVu Sans" };
+static const char dmenufont[]       = "DejaVuSansMono Nerd Font:style=Regular:size=9";
 static const char col_gray1[]       = "#000000";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#cccccc";
@@ -46,6 +47,7 @@ static const char *colors[][3]      = {
 
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9", "0" };
+static const int initlayouts[] = { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2 };
 
 static const Rule rules[] = {
 	/* xprop(1):
@@ -57,7 +59,7 @@ static const Rule rules[] = {
 	{ "Firefox",  		NULL,       	NULL,	1<<8,	 	0,			-1,			50,50,500,500,		2 },
 	{ "calculator", 	NULL,			NULL,	0,		 	1,			-1,			80,50,400,300,		2 },
 	{ "floating",		NULL,			NULL,	0,		 	1,			-1,			80,50,800,600,		0 },
-	{ "scratchpad",		NULL,			NULL,	0,		 	1,			-1,			80,50,995,668,		0 },
+	{ "scratchpad",		NULL,			NULL,	0,		 	1,			-1,			80,50,1366,900,		0 },
 	{ "qBittorrent",	"qbittorrent",	NULL,	1<<8,    	0,			-1,			80,50,800,600,      0 },
 	{ "Spotify",        "spotify",      NULL,       1<<8,           0,           -1,        80,50,800,600,      0 },
 	{ NULL,             NULL,           "Spotify",  1<<8,           0,           -1,        80,50,800,600,      0 },
@@ -154,6 +156,10 @@ static Key keys[] = {
 	{ MODKEY,                       XK_period, focusmon,       {.i = +1 } },
 	{ MODKEY|ShiftMask,             XK_comma,  tagmon,         {.i = -1 } },
 	{ MODKEY|ShiftMask,             XK_period, tagmon,         {.i = +1 } },
+	{ MODKEY,                       XK_minus,  setgaps,        {.i = -5 } },
+	{ MODKEY,                       XK_equal,  setgaps,        {.i = +5 } },
+	{ MODKEY|ShiftMask,             XK_minus,  setgaps,        {.i = GAP_RESET } },
+	{ MODKEY|ShiftMask,             XK_equal,  setgaps,        {.i = GAP_TOGGLE} },
 	TAGKEYS(                        XK_1,                      0)
 	TAGKEYS(                        XK_2,                      1)
 	TAGKEYS(                        XK_3,                      2)
